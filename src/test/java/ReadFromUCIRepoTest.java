521779a3e7a57b3a135ad3e2dee36a5cf79cecd5
joshcrasto <jcrasto@g.ucla.edu>
2020-05-24 14:07:05 -0500

    read from data url, create file locally and delete

--- src/test/java/ReadFromUCIRepoTest.java
+++ src/test/java/ReadFromUCIRepoTest.java
@@ -4,8 +4,9 @@ public class ReadFromUCIRepoTest {
     @Test
     public void test() throws Exception{
 //        String[] args = null;
-        String[] args = new String [] {"https://archive.ics.uci.edu/ml/machine-learning-databases/adult/"};
-//        String[] args = new String [] {"https://archive.ics.uci.edu/ml/machine-learning-databases/iris"};
+//        String[] args = new String [] {"https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"};
+        String[] args = new String [] {"https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"};
+//        System.out.println("Working Directory = " + System.getProperty("user.dir"));
         ReadFromUCIRepo.main(args);
     }
 }
